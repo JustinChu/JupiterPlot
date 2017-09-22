632bbacdce8eaa5cb245100aa9c16da3a8c61de3
JustinChu <cjustin@bcgsc.ca>
2017-09-22 09:56:24 -0700

    make mutlifasta file scaffolds work

--- bin/fatoagp.pl
+++ bin/fatoagp.pl
@@ -9,23 +9,31 @@ use Getopt::Std qw'getopts';
 my %opt;
 getopts 'f:s:S:', \%opt;
 my $opt_fasta = $opt{'f'};
+
 # scaffolds shorter than this length will be excluded
 my $opt_min_scaf_len = defined $opt{'s'} ? $opt{'s'} : 200;
+
 # scaftigs shorter than this length will be masked with "N"s
 my $opt_min_ctg_len = defined $opt{'S'} ? $opt{'S'} : 50;
 
 open FASTA, ">$opt_fasta"
-	or die "error: `$opt_fasta': $!\n"
-	if $opt_fasta;
+  or die "error: `$opt_fasta': $!\n"
+  if $opt_fasta;
 
-while (<>) {
-	die unless /^>/;
-	chomp;
-	my ($scafid, undef) = split ' ', $_, 2;
+my $currentStr = <>;
+
+while ($currentStr) {
+	chomp $currentStr;
+	my ( $scafid, undef ) = split ' ', $currentStr, 2;
 	substr $scafid, 0, 1, '';
 
-	my $scafseq = <>;
-	chomp $scafseq;
+	$currentStr = <>;
+	my $scafseq = "";
+	while ( $currentStr && $currentStr !~ /^>/ ) {
+		chomp $currentStr;
+		$scafseq .= $currentStr;
+		$currentStr = <>;
+	}
 	my $scaflen = $scafseq =~ tr/ACGTacgt//;
 	next if $scaflen < $opt_min_scaf_len;
 
@@ -33,10 +41,11 @@ while (<>) {
 	my @ctgseqs = split /([Nn]+)/, $scafseq;
 	foreach my $ctgseq (@ctgseqs) {
 		next if /^[nN]/;
-		if (length($ctgseq) < $opt_min_ctg_len) {
+		if ( length($ctgseq) < $opt_min_ctg_len ) {
 			$ctgseq = "N" x length($ctgseq);
 		}
 	}
+
 	# rejoin and split to merge adjacent stretches of "N"s
 	$scafseq = join '', @ctgseqs;
 	next unless $scafseq =~ /[^nN]/;
@@ -46,23 +55,27 @@ while (<>) {
 	my $x = 0;
 	for my $ctgseq (@ctgseqs) {
 		my $len = length $ctgseq;
-		$i++ if ($len == 0);
-		next if ($len == 0);
+		$i++ if ( $len == 0 );
+		next if ( $len == 0 );
+
 		# object object_beg object_end part_number
 		print 'scaffold', $scafid, "\t",
-			$x + 1, "\t",
-			$x + $len, "\t",
-			$i + 1, "\t";
-		if ($ctgseq =~ /^[nN]/) {
+		  $x + 1, "\t",
+		  $x + $len, "\t",
+		  $i + 1, "\t";
+		if ( $ctgseq =~ /^[nN]/ ) {
+
 			# component_type gap_length gap_type linkage
 			print "N\t", $len, "\tscaffold\tyes\tpaired-ends\n";
-		} else {
-			my $ctgid = 'contig' . $scafid . '_' . ($i / 2);
+		}
+		else {
+			my $ctgid = 'contig' . $scafid . '_' . ( $i / 2 );
+
 			# component_type component_id
 			# component_beg component_end orientation
 			print "W\t", $ctgid, "\t1\t", $len, "\t+\n";
 			print FASTA '>', $ctgid, "\n", $ctgseq, "\n"
-				if $opt_fasta;
+			  if $opt_fasta;
 		}
 		$i++;
 		$x += $len;
