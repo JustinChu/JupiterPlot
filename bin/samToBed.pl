48b76daf4bde4956c98721e86325e62785361025
JustinChu <cjustin@bcgsc.ca>
2017-09-25 16:10:06 -0700

    don't use bitwise awk and() for bedfile generation

---
+++ bin/samToBed.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+
+#Written by Justin Chu 2017
+#generate a bedfile given sam file format
+
+use strict;
+use warnings;
+
+my %chrlengths;
+my %bandStr;
+
+while (<>) {
+	my @line = split( /\t/, $_ );
+	print "$line[2]\t$line[3]\t"
+	  . ( $line[3] + length( $line[9] ) )
+	  . "\t$line[0]\t$line[4]\t";
+	if ( $line[1] & 16 == 0 ) {
+		print "+\n";
+	}
+	else {
+		print "-\n";
+	}
+}
