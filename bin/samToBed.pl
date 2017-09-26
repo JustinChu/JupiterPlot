5a536570fbea4476077537ac7ea9944f66d36e9e
JustinChu <cjustin@bcgsc.ca>
2017-09-26 16:38:58 -0700

    print out actual bounds of bed file using cigar string

--- bin/samToBed.pl
+++ bin/samToBed.pl
@@ -11,13 +11,28 @@ my %bandStr;
 
 while (<>) {
 	my @line = split( /\t/, $_ );
-	print "$line[2]\t$line[3]\t"
-	  . ( $line[3] + length( $line[9] ) )
-	  . "\t$line[0]\t$line[4]\t";
+	print $line[2] . "\t"
+	  . ( $line[3] - 1 ) . "\t"
+	  . ( $line[3] - 1 + getPaddedReferenceLength( $line[5] ) ) . "\t"
+	  . $line[0] . "\t"
+	  . $line[4] . "\t";
 	if ( $line[1] & 16 == 0 ) {
 		print "-\n";
 	}
 	else {
 		print "+\n";
 	}
+}
+
+#parse from cigar string
+sub getPaddedReferenceLength {
+	my $cigar  = shift;
+	my $length = 0;
+	if ( $cigar =~ /\d+M|D|N|EQ|X|P/ ) {
+		my @elements = ( $cigar =~ /(\d+)(?:M|D|N|EQ|X|P)/g );
+		for my $value (@elements) {
+			$length += $value;
+		}
+	}
+	return $length;
 }
