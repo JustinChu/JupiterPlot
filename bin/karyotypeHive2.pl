f0ab0ecf3d646fb68e07ec8f14715cc7d5e3c805
cjustin <cjustin@bcgsc.ca>
2020-03-26 10:40:49 -0700

    bugfix

--- bin/karyotypeHive2.pl
+++ bin/karyotypeHive2.pl
@@ -310,7 +310,7 @@ sub printAxisStr {
 	my $reverse    = shift;
 	my $segmentStr = shift;
 	print $fd
-"<axis $prefix>\nangle = $angle\nscale = 1\nreverse = $reverse\nsegements = $segmentStr\n</axis>\n";
+"<axis $prefix>\nangle = $angle\nscale = 1\nreverse = $reverse\nsegments = $segmentStr\n</axis>\n";
 
 }
 
